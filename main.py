eb2f526f66e7e0aa5d84f50bd3e6dc54ad42da1c
Niakr1s <pavel2188@gmail.com>
2025-05-02 14:35:42 +0300

    added concurrent download with limit

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ from civitai.network.infinite import InfiniteLoader, Input
 from civitai.network.parser import extract_mantine_class
 
 
-async def download_file(session: aiohttp.ClientSession, id: int, out_dir: str):
-    image_html = await get_image_html(session, id)
-    url = extract_mantine_class(image_html)[0]
-    start = time()
-    saved_path, downloaded = await File.download(session, url, out_dir, f"{id}")
-    duration = time() - start
-    msg = "Save" if downloaded else "Skip"
-    print(f"[{id}] {msg} '{saved_path}' in {duration:.2f}s")
+async def download_file(
+    session: aiohttp.ClientSession, id: int, out_dir: str, sem: asyncio.Semaphore
+):
+    async with sem:
+        image_html = await get_image_html(session, id)
+        url = extract_mantine_class(image_html)[0]
+        start = time()
+        saved_path, downloaded = await File.download(session, url, out_dir, f"{id}")
+        duration = time() - start
+        msg = "Save" if downloaded else "Skip"
+        print(f"[{id}] {msg} '{saved_path}' in {duration:.2f}s")
 
 
-async def download_n_files(n: int, out_dir: str, tags: list[int] | None = None):
+async def download_n_files(
+    n: int, out_dir: str, tags: list[int] | None = None, max_workers: int = 5
+):
     async with aiohttp.ClientSession(cookies=cookies) as session:
         infinite_loader = InfiniteLoader(session)
 
@@ -29,10 +34,13 @@ async def download_n_files(n: int, out_dir: str, tags: list[int] | None = None):
             input = input.next(page.nextCursor)  # update input immediatly
             print(f"Loaded {len(page.ids)} ids. {n} files left to download.")
 
+            batch = []
+            sem = asyncio.Semaphore(max_workers)  # limit number of concurrent downloads
             while len(page.ids) > 0 and n > 0:
                 id = page.ids.pop(0)
-                await download_file(session, id, out_dir)
+                batch.append(download_file(session, id, out_dir, sem=sem))
                 n -= 1
+            await asyncio.gather(*batch)
 
 
 if __name__ == "__main__":
